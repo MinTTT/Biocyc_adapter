fdea0e5e22c4638a11ac4e4093132be49d1ece6b
MinTTT <pan_chu@outlook.com>
2023-12-02 21:58:49 +0800

    amend

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ command_lib = {
 
 # ====================
 # Find Tables in biocyc: https://biocyc.org/gene-search.shtml
+# https://biocyc.org/getxml?id=ECOLI:PM0-1352&detail=low
+# https://biocyc.org/getxml?ECOLI:RPOH-MONOMER
 # %%
 
 
@@ -28,9 +30,8 @@ s = requests.Session()  # create session
 # Post login credentials to session:
 s.post('https://websvc.biocyc.org/credentials/login/', data={'email': account, 'password': psw})
 # Issue web service request:
-# %%
 r = s.get('https://websvc.biocyc.org/kb-version?orgid=ECOLI')
-
+# %% get all gene
 genes = s.get(f'{webServer}/ECOLI/class-instances?object=Genes')
 genes_soup = BeautifulSoup(genes.text, "lxml")
 tables = genes_soup.find_all('table')
@@ -55,6 +56,53 @@ for gene_row in genes_web_table.find_all('a'):
 genes_table = pd.DataFrame(data=genes_dict)
 genes_table.to_csv(r'./exported_data/Ecoli_Genes.csv')
 genes_table.to_excel(r'./exported_data/Ecoli_Genes.xlsx')
+# %% get all RNAs
+genes = s.get(f'{webServer}/ECOLI/class-instances?object=RNAs')
+genes_soup = BeautifulSoup(genes.text, "xml")
+tables = genes_soup.find_all('table')
+for table in tables:
+    if 'class' in list(table.attrs.keys()):
+        if table.attrs['class'][0] == 'sortableSAQPoutputTable':
+            genes_web_table = table
+genes_dict = dict(
+    genes_name=[],
+    genes_id=[],
+    genes_href=[],
+    RNA_name=[],
+    RNA_id=[],
+    RNA_href=[],
+)
+
+for gene_row in genes_web_table.find_all('tr'):
+    # gene_row = gene_row.find_all('td')
+    td = gene_row.find_all('a')
+    if len(td) != 0:
+        if len(td) == 1:
+            obj = td[0]
+            gene_name = None
+            gene_href = None
+            gene_id = None
+
+        else:
+            obj, gene = td
+            gene_name = gene.text
+            gene_href = biocyc_web + gene.attrs['href']
+            gene_id = gene_href.split('=')[-1]
+        object_name = obj.text
+        object_href = biocyc_web + obj.attrs['href']
+        object_id = object_href.split('=')[-1]
+
+        genes_dict['genes_name'].append(gene_name)
+        genes_dict['genes_href'].append(gene_href)
+        genes_dict['genes_id'].append(gene_id)
+        genes_dict['RNA_name'].append(object_name)
+        genes_dict['RNA_id'].append(object_id)
+        genes_dict['RNA_href'].append(object_href)
+
+
+genes_table = pd.DataFrame(data=genes_dict)
+genes_table.to_csv(r'./exported_data/Ecoli_RNAs.csv')
+genes_table.to_excel(r'./exported_data/Ecoli_RNAs.xlsx')
 
 # %% Retrieve all gene info
 import time
@@ -81,16 +129,13 @@ for g_id in tqdm(genes_dict['genes_id']):
     thread_gene_retrieve = Thread(target=request_info, args=(g_id, info_dict))
     thread_gene_retrieve.start()
 
-# back up gene info
+    # back up gene info
     for g_id, g_info in info_dict.items():
-
         g_name = genes_table[genes_table['genes_id'] == g_id]['genes_name'].tolist()[0]
         with open(f'./exported_data/Genes_info_xml/{g_id}_{g_name}.xml', 'w') as g_xml:
-
             g_xml.write(g_info)
 
-
-#%% load gene xlm files
+# %% load gene xlm files
 
 genes_table = pd.read_csv(r'.\exported_data\Ecoli_Genes.csv')
 genes_id = genes_table['genes_id'].to_list()
@@ -105,7 +150,38 @@ for gene_xml in genes_file:
             with open(f'./exported_data/Genes_info_xml/{file_name}', 'r') as g_xml:
                 info_dict[gene_id] = g_xml.read()
 
+gene_soup = BeautifulSoup(info_dict['EG10001'], 'xml')
 
-gene_soup = BeautifulSoup(info_dict['EG10001'], 'lxml')
+
+def get_gene_info(gene_xml):
+    """
+    product
+    transcription-direction
+    synonym: <synonym>
+    region: <left_end_position, right_end_position>
+    name: <common-name>
+    Parameters
+    ----------
+    gene_xml :
+
+    Returns
+    -------
+
+    """
+    gene_soup = BeautifulSoup(gene_xml, 'xml')
+    product = gene_soup.find('product').findChild().name
+    transcription_direction = gene_soup.find('transcription-direction').text
+    left_end_position = int(gene_soup.find('left-end-position').text)
+    right_end_position = int(gene_soup.find('right-end-position').text)
+    region = (left_end_position, right_end_position)
+    synonym = gene_soup.find('synonym').text
+    info_dict = {
+        'transcription-direction': transcription_direction,
+        'product': product,
+        'region': region,
+        'synonym': synonym
+    }
+    return info_dict
 
 
+get_gene_info(info_dict['EG10001'])
